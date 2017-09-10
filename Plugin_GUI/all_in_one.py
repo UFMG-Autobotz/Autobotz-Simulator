a9e31cd3eed5e140a44c00ff6195c4b925600e94
Rodrigo Cézar <rodrigocezarluz@gmail.com>
2017-09-10 14:00:04 -0300

    Separar em arquivos diferentes

--- Plugin_GUI/all_in_one.py
+++ Plugin_GUI/all_in_one.py
@@ -67,44 +67,31 @@ class Main_Window(QtGui.QMainWindow):
 
 		self.setWindowTitle("Main Window")
 
+	def new_Dock(self, name):
+		dock = QtGui.QDockWidget(name, self)
+		dock.setAllowedAreas(QtCore.Qt.TopDockWidgetArea)
+		dock.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+		dock_title = DockTitleBar(dock)
+		self.addDockWidget(QtCore.Qt.TopDockWidgetArea, dock)
+		self.dockList.append( dock )
+
+		# if len(self.dockList) > 1:
+		# 	self.tabifyDockWidget(self.dockList[-2],self.dockList[-1])
+
 	def controller_action(self,q):
 		if q.text() == 'Nova aba':
-			dock = QtGui.QDockWidget('Slider', self)
-			dock.setWidget(Sliders_Window(self.slider_groups_file, dock))
-			dock.setAllowedAreas(QtCore.Qt.TopDockWidgetArea)
-			dock.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-			dock_title = DockTitleBar(dock)
-			self.addDockWidget(QtCore.Qt.TopDockWidgetArea, dock)
-			self.dockList.append( dock )
-
-			# if len(self.dockList) > 1:
-			# 	self.tabifyDockWidget(self.dockList[-2],self.dockList[-1])
+			self.new_Dock('Slider')
+			self.dockList[-1].setWidget(Sliders_Window(self.slider_groups_file, self.dockList[-1]))
 
 	def graph_Plotter_action(self,q):
 		if q.text() == 'Nova aba':
-			dock = QtGui.QDockWidget('Graph', self)
-			dock.setWidget( Graphs_Window(dock))
-			dock.setAllowedAreas(QtCore.Qt.TopDockWidgetArea)
-			dock.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-			dock_title = DockTitleBar(dock)
-			self.addDockWidget(QtCore.Qt.TopDockWidgetArea, dock)
-			self.dockList.append( dock )
-
-			# if len(self.dockList) > 1:
-			# 	self.tabifyDockWidget(self.dockList[-2],self.dockList[-1])
+			self.new_Dock('Graph')
+			self.dockList[-1].setWidget(Graphs_Window(self.dockList[-1]))
 
 	def image_Viewer_action(self,q):
 		if q.text() == 'Nova aba':
-			dock = QtGui.QDockWidget('Image', self)
-			dock.setWidget(Image_Window(dock))
-			dock.setAllowedAreas(QtCore.Qt.TopDockWidgetArea)
-			dock.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-			dock_title = DockTitleBar(dock)
-			self.addDockWidget(QtCore.Qt.TopDockWidgetArea, dock)
-			self.dockList.append( dock )
-
-			# if len(self.dockList) > 1:
-			# 	self.tabifyDockWidget(self.dockList[-2],self.dockList[-1])
+			self.new_Dock('Image')
+			self.dockList[-1].setWidget(Image_Window(self.dockList[-1]))
 
 class DockTitleBar(QtGui.QWidget):
 	def __init__(self, dockWidget):
