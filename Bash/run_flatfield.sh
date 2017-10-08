97983a40725fb6cd1e5deef261cfc7a57d13bd1a
Barbara <babialmeida1508@gmail.com>
2017-10-08 16:38:59 -0300

    add testes futebol

--- Bash/run_flatfield.sh
+++ Bash/run_flatfield.sh
@@ -5,7 +5,7 @@ erb Models/VT_simplificado/model.rsdf > Models/VT_simplificado/model.sdf
 
 # adicionar caminho para a textura
 source /usr/share/gazebo/setup.sh
-export GAZEBO_RESOURCE_PATH=$(pwd)/Textures:$GAZEBO_RESOURCE_PATH
+export GAZEBO_RESOURCE_PATH=$(pwd)/Materials/Textures:$GAZEBO_RESOURCE_PATH
 
 # adicionar caminho para o modelo
 export GAZEBO_MODEL_PATH=$(pwd)/Models:$GAZEBO_MODEL_PATH
--- Bash/run_vsstest.sh
+++ Bash/run_vsstest.sh
@@ -1,17 +1,18 @@
 cd ..
 
 # salvar .sdf a partir do .rsdf parametrizavel
-erb Models/VT_simplificado/model.rsdf > Models/VT_simplificado/model.sdf
+erb Models/VSS_Player/model.rsdf > Models/VSS_Player/model.sdf
 
-# adicionar caminho para a textura
+# adicionar caminho para a materiais
 source /usr/share/gazebo/setup.sh
-export GAZEBO_RESOURCE_PATH=$(pwd)/Materials/Textures:$GAZEBO_RESOURCE_PATH
+export GAZEBO_RESOURCE_PATH=$(pwd)/Materials:$GAZEBO_RESOURCE_PATH
 
 # adicionar caminho para o modelo
 export GAZEBO_MODEL_PATH=$(pwd)/Models:$GAZEBO_MODEL_PATH
 
 # adicionar caminho para o plugin
 export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/PID_Control/import:${GAZEBO_PLUGIN_PATH}
+export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/camera_plugin/import:${GAZEBO_PLUGIN_PATH}
 
 # iniciar gazebo com mundo parado
-gazebo Worlds/vt_bowlfield.world -u --verbose
+gazebo Worlds/vss_test.world -u --verbose
