af6435c1759753453a88bd5d6c6fbd301d52db4d
Barbara <babialmeida1508@gmail.com>
2017-10-12 09:38:09 -0300

    test ball with vss player

--- Bash/run_vsstest.sh
+++ Bash/run_vsstest.sh
@@ -2,15 +2,16 @@ cd ..
 
 # salvar .sdf a partir do .rsdf parametrizavel
 erb Models/VSS_Player/model.rsdf > Models/VSS_Player/model.sdf
+erb Models/VSS_Ball/model.rsdf > Models/VSS_Ball/model.sdf
 
 # adicionar caminho para a materiais
 source /usr/share/gazebo/setup.sh
 export GAZEBO_RESOURCE_PATH=$(pwd)/Materials:$GAZEBO_RESOURCE_PATH
 
-# adicionar caminho para o modelo
+# adicionar caminho para os modelos
 export GAZEBO_MODEL_PATH=$(pwd)/Models:$GAZEBO_MODEL_PATH
 
-# adicionar caminho para o plugin
+# adicionar caminho para os plugins
 export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/PID_Control/import:${GAZEBO_PLUGIN_PATH}
 export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/camera_plugin/import:${GAZEBO_PLUGIN_PATH}
 
