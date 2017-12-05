6f4b5fb59537c5b7b5e93f4664faf83fb4f5b900
Barbara <babialmeida1508@gmail.com>
2017-12-05 21:23:09 -0200

    rename VT_field_bowl, estava com tag VSS por engano

--- Bash/Setup/VT_setup.sh
+++ Bash/Setup/VT_setup.sh
@@ -3,13 +3,13 @@ cd ..
 # salvar .sdf a partir do .rsdf parametrizavel
 erb Models/VT_vt_simplificado/model.rsdf > Models/VT_vt_simplificado/model.sdf
 
-# adicionar caminho para a textura
+# adicionar caminho para as texturas
 source /usr/share/gazebo/setup.sh
 export GAZEBO_RESOURCE_PATH=$(pwd)/Materials/Textures:$GAZEBO_RESOURCE_PATH
 
-# adicionar caminho para o modelo
+# adicionar caminho para os modelos
 export GAZEBO_MODEL_PATH=$(pwd)/Models:$GAZEBO_MODEL_PATH
 
-# adicionar caminho para o plugin
+# adicionar caminho para os plugins
 export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/GENERIC_pid_control/import:${GAZEBO_PLUGIN_PATH}
 export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/GENERIC_camera_ros/import:${GAZEBO_PLUGIN_PATH}
