6d8ee8180cd0c45178fd32402a20041771448e16
Barbara <babialmeida1508@gmail.com>
2017-12-23 10:31:29 -0200

    funçoes que precisam do dataparser comentadas para poder compilar

--- Plugins/GENERIC_debug_link/DebugLinkPlugin.cc
+++ Plugins/GENERIC_debug_link/DebugLinkPlugin.cc
@@ -37,46 +37,46 @@ void DebugLinkPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
 /*-------------------*/
 
 void DebugLinkPlugin::setTopics() {
-	int linkCount = this->link_data->GetLinkCount();  // get number of valid links
-
-	link_param *currentLink;
-
-	this->rosPub_vector.resize(linkCount);
-	for (int i = 0; i < linkCount; i++) {
-		currentLink = this->link_data->GetLink(i);
-
-		if(currentLink->valid && currentLink->pose) {
-			 this->rosPub_vector[i] = this->rosNode->advertise<geometry_msgs::Pose>(currentLink->postopic, 100);
-		}
-	}
+	// int linkCount = this->link_data->GetLinkCount();  // get number of valid links
+  //
+	// link_param *currentLink;
+  //
+	// this->rosPub_vector.resize(linkCount);
+	// for (int i = 0; i < linkCount; i++) {
+	// 	currentLink = this->link_data->GetLink(i);
+  //
+	// 	if(currentLink->valid && currentLink->pose) {
+	// 		 this->rosPub_vector[i] = this->rosNode->advertise<geometry_msgs::Pose>(currentLink->postopic, 100);
+	// 	}
+	// }
 
 }
 
 /*-------------------*/
 
 void DebugLinkPlugin::OnUpdate() {
-	int linkCount = this->link_data->GetLinkCount();  // get number of valid links
-	link_param *currentLink;
-	math::Pose linkPose;
-
-	for (int i = 0; i < linkCount; i++) {
-		currentLink = this->link_data->GetLink(i);
-		if(currentLink->valid && currentLink->pose) {
-		  linkPose = currentLink->link->GetWorldCoGPose();
-
-			geometry_msgs::Pose currentLink_pose;
-
-			currentLink_pose.position.x = linkPose.pos.x;
-			currentLink_pose.position.y = linkPose.pos.y;
-			currentLink_pose.position.z = linkPose.pos.z;
-
-			currentLink_pose.orientation.x =linkPose.rot.x;
-			currentLink_pose.orientation.y =linkPose.rot.y;
-			currentLink_pose.orientation.z =linkPose.rot.z;
-			currentLink_pose.orientation.w =linkPose.rot.w;
-
-			this->rosPub_vector[i].publish(currentLink_pose);
-		}
-	}
+	// int linkCount = this->link_data->GetLinkCount();  // get number of valid links
+	// link_param *currentLink;
+	// math::Pose linkPose;
+  //
+	// for (int i = 0; i < linkCount; i++) {
+	// 	currentLink = this->link_data->GetLink(i);
+	// 	if(currentLink->valid && currentLink->pose) {
+	// 	  linkPose = currentLink->link->GetWorldCoGPose();
+  //
+	// 		geometry_msgs::Pose currentLink_pose;
+  //
+	// 		currentLink_pose.position.x = linkPose.pos.x;
+	// 		currentLink_pose.position.y = linkPose.pos.y;
+	// 		currentLink_pose.position.z = linkPose.pos.z;
+  //
+	// 		currentLink_pose.orientation.x =linkPose.rot.x;
+	// 		currentLink_pose.orientation.y =linkPose.rot.y;
+	// 		currentLink_pose.orientation.z =linkPose.rot.z;
+	// 		currentLink_pose.orientation.w =linkPose.rot.w;
+  //
+	// 		this->rosPub_vector[i].publish(currentLink_pose);
+	// 	}
+	// }
 
 }
